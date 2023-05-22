e81389d5a9463013323a0f46c0fa5a6045aa134f
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-05-22 18:42:19 -0300

    Collections: set

---
+++ Java/JavaOO/Aula04/Aulla04/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Set representa um CONJUNTO de dados.
+        NENHUM ELEMENTO PODE SER IGUAL nessa estrutura de dados.
+        NÃO GARANTE UMA ORDEM ESPECÍFICA dos elementos, mas oferece uma boa performance de BUSCA e INSERÇÃO
+         */
+
+        Set<String> set = new HashSet<>(); //um conjunto de strings
+
+        //principais métodos
+        //set elemento
+        set.add("Maçã");
+        set.add("Laranja");
+
+        //Removendo um elemento do set
+        set.remove("Maçã");
+
+        //Verificando se o set contém um elemento específico
+        boolean contem = set.contains("Banana");
+
+        //Imprimindo o set atualizado
+        System.out.println(set);
+
+        //Iterando sobre os elementos do set
+        System.out.println("Elementos do set:");
+        for (String elemento : set) {
+            System.out.println(elemento);
+        }
+        //Limpando o set
+        set.clear();
+
+        //Verifica se o set está vazio
+        System.out.println("O set está vazio? " + set.isEmpty());
+
+    }
+}
